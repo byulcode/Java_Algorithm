731976962e9936004c68dc238ff218475c9190ee
bybit0408 <bybit0408@gmail.com>
2021-07-14 23:56:32 +0900

    #18870 modify

--- src/Baekjoon/Sort1.java
+++ src/Baekjoon/Sort1.java
@@ -1,10 +1,15 @@
 package Baekjoon;
 //백준 #18870번 좌표압축 
+/*좌표압축 : 좌표의 범위가 너무 큰 경우 인덱싱으로 좌표 사이 갭을 없애는 기법
+           데이터를 정렬해서 다시 순서를 부여하는것(중복 제외)
+필요한 이유 :메모리를 줄이기 위해
+#깊은 복사(deep copy)
+배열을 복사한 후에 한쪽 값을 수정해도 다른 배열에 영향 끼치지 않음
+1차원 배열의 깊은복사 ==> 배열.clone()
+*/
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 public class Sort1 {
@@ -13,8 +18,12 @@ public class Sort1 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());	//N의 값
 		StringBuilder sb = new StringBuilder();		
+		
 		String[] array = bf.readLine().split(" ");
-		int[] nums = Arrays.stream(array).mapToInt(Integer::parseInt).toArray();	//string[] --> int[]
+		int[] nums = new int[N];
+		for(int i=0;i<array.length;i++) 
+			nums[i] = Integer.parseInt(array[i]);
+		
 		int cnt = 0;
 		
 		int numClone[] = nums.clone();	//복사(출력시 사용)
@@ -29,9 +38,6 @@ public class Sort1 {
 			sb.append(hash.get(numClone[i])).append(" ");
 		}
 		
-		System.out.println(sb.toString());
-		
-		
+		System.out.print(sb.toString());
 	}
-
 }
---
+++ src/Baekjoon/Sort2.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+//#2750 수 정렬하기
+public class Sort2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
